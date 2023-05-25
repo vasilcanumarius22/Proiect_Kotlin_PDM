f57bfe58e56e2fc93f09ceaf118994aaa87c50e5
Marius Daniel Vasilcanu <114442713+vasilcanumarius22@users.noreply.github.com>
2023-05-26 00:29:47 +0300

    update

--- Kotlin-Project_PDM/app/src/main/java/com/example/kotlin_project_pdm/WeatherActivity.kt
+++ Kotlin-Project_PDM/app/src/main/java/com/example/kotlin_project_pdm/WeatherActivity.kt
@@ -1,17 +1,27 @@
 package com.example.kotlin_project_pdm
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin_project_pdm.ui.weather.WeatherViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.Locale
 
 class WeatherActivity : AppCompatActivity() {
     private lateinit var viewModel: WeatherViewModel
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +29,34 @@ class WeatherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_weather)
 
         viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         val cityInput = findViewById<EditText>(R.id.cityInput)
         val fetchButton = findViewById<Button>(R.id.fetchButton)
+        val locationButton = findViewById<Button>(R.id.locationButton) // Add this line
         val weatherText = findViewById<TextView>(R.id.weatherText)
         val cityNameText = findViewById<TextView>(R.id.cityNameText)
 
         fetchButton.setOnClickListener {
             viewModel.fetchWeather(cityInput.text.toString())
+        }
+
+        locationButton.setOnClickListener { // Add this code block
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Request location permission
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    0
+                )
+            } else {
+                getLastLocation()
+            }
         }
 
         viewModel.weatherLiveData.observe(this) { weather ->
@@ -43,7 +73,6 @@ class WeatherActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
@@ -54,5 +83,64 @@ class WeatherActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-}
 
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                try {
+                    val geocoder = Geocoder(this, Locale.getDefault())
+                    val addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                    addresses?.let { addr ->
+                        if (addr.isNotEmpty()) {
+                            val cityName = addr[0].locality ?: return@let
+                            viewModel.fetchWeather(cityName)
+                        }
+                    }
+                } catch (exception: Exception) {
+                    exception.printStackTrace()
+                    // Handle the error accordingly
+                }
+            }
+        }
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            0 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted, get the location
+                    getLastLocation()
+                } else {
+                    // Permission denied, disable the functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+}
